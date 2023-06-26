af8049cc498f5f574056f77cf711ace4e51a82d4
yllberishastudent <94076526+yllberishastudent@users.noreply.github.com>
2023-06-27 05:40:59 +0800

    Initialising the project

---
+++ projektiMobile2023/projektiMobile2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  projektiMobile2023
+//
+//  Created by mady on 27/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ projektiMobile2023/projektiMobile2023/tableCell.swift
@@ -0,0 +1,13 @@
+//
+//  tableCell.swift
+//  projektiMobile2023
+//
+//  Created by mady on 27/06/2023.
+//
+
+import Foundation
+import UIKit
+
+class tableCell : UITableViewCell {
+    
+}
